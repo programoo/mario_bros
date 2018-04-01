34e272c360a0b82942ee3322a46af521bfa4cb27
programoo <prakarnwongsanit@gmail.com>
2018-04-01 16:29:01 +0700

    MB-0000: Change screen viewport to be auto-adjusted

--- core/src/com/programoo/mariobros/screens/PlayScreen.java
+++ core/src/com/programoo/mariobros/screens/PlayScreen.java
@@ -3,16 +3,31 @@ package com.programoo.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.programoo.mariobros.MarioBros;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game){
         this.game = game;
         texture = new Texture("badlogic.jpg");
+
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gamecam);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
     }
 
     @Override
@@ -25,10 +40,7 @@ public class PlayScreen implements Screen {
         clearScreen();
     }
 
-    @Override
-    public void resize(int width, int height) {
 
-    }
 
     @Override
     public void pause() {
@@ -53,6 +65,9 @@ public class PlayScreen implements Screen {
     private void clearScreen(){
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(gamecam.combined);
+
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
